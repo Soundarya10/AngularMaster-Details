5755df2284c9dc90d19332c5bafd0ae3e924c630
Soundarya <sreddy.0720@gmail.com>
2018-01-22 21:12:47 -0500

    updated version

--- Js/tabController.js
+++ Js/tabController.js
@@ -21,23 +21,71 @@
 
             $scope.selectedRow = null;
 
-        $scope.openDetails = function(x) {
-            $scope.selectedRow = x;
-            $scope.fname = x.Fname;
-            $scope.lname = x.Lname;
-            $scope.age = x.Age;
+        $scope.openDetails = function(index) {
+            $scope.selectedRow = index;
+            $scope.fname = $scope.myTableData[index].Fname;
+            $scope.lname = $scope.myTableData[index].Lname;
+            $scope.age = $scope.myTableData[index].Age;
 }
 
-     $scope.openDetails1 = function(x) {
-            $scope.selectedRow = x;
-            console.log(x);
-            $scope.fname = x.Fname;
-            $scope.lname = x.Lname;
-            $scope.age = x.Age;
-}
+ $scope.setClickedRow = function(index) {
+    $scope.selectedRow = index;
+  }
+  
+  $scope.moveUp = function(num) {
+    if (num > 0) {
+     var tmp = $scope.myTableData[num - 1];
+            $scope.fname = tmp.Fname;
+            $scope.lname = tmp.Lname;
+            $scope.age = tmp.Age;
+      $scope.selectedRow--;
+    }
+  }
+  $scope.moveDown = function(num) {
+    if (num < $scope.myTableData.length - 1) {
+      var tmp = $scope.myTableData[num + 1];
+            $scope.fname = tmp.Fname;
+            $scope.lname = tmp.Lname;
+            $scope.age = tmp.Age;
+      $scope.selectedRow++;
+    }
+  }
 
+  $scope.makeItEditable=function(index){
+    if($scope.currentEditableIndex!=null)
+    $scope.myTableData[$scope.currentEditableIndex].editable=false;
+    $scope.myTableData[index].editable=true;
+    $scope.currentEditableIndex=index;
+    console.log($scope.myTableData[index].editable);
 
-        
+    }
+
+     $scope.deleteIt=function(index){
+    if($scope.currentEditableIndex!=null)
+    $scope.myTableData[$scope.currentEditableIndex].editable=false;
+    $scope.myTableData.splice(index,1);
+    $scope.currentEditableIndex=null;
+    console.log($scope.myTableData);
+
+    }
+
+    $scope.addRow = function(){     
+    $scope.myTableData.push({ 'Fname':$scope.firstname, 'Lname': $scope.lastname,
+     'Age':$scope.PersonAge1 });
+    $scope.firstname='';
+    $scope.lastname='';
+    $scope.PersonAge1='';
+};
+
+$scope.editRow = function(index){  
+    $scope.myTableData.push({ 'Fname':$scope.fname, 'Lname': $scope.lname,
+     'Age':$scope.age });
+    $scope.fname='';
+    $scope.lname='';
+    $scope.age='';
+    $scope.deleteIt(index);
+};
+
     }
 
 })();
--- config.js
+++ config.js
@@ -4,11 +4,8 @@
 
     app.config(['$routeProvider', function($routeProvider) {
         $routeProvider.
+        
         when('/', {
-            templateUrl: './views/tableView.html',
-            controller: 'tableCtrl',
-        }).
-        when('/tab', {
             templateUrl: './views/tabView.html',
             controller: 'tabCtrl',
         }).
